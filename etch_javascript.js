ac2f99950e70106f9a51daa779b3ce48ee0bda35
ctaylor9107 <coleman.raphael.taylor@gmail.com>
2025-03-08 20:42:35 -0600

    Add logic to change color of grid elements
    Added code that changes the color of one of the grid elements when the
    cursor moves into it.  This then creates a trail of the grid elements
    that it moves through.

--- etch_javascript.js
+++ etch_javascript.js
@@ -2,14 +2,19 @@
 const container = document.querySelector("#container");
 let gridAmount = 256;
 
-//const grid = document.createElement("div");
-//grid.classList.add("grid");
 
-//container.appendChild(grid);
 
-for (i = 0; i <= gridAmount; i++) {
+for (i = 0; i < gridAmount; i++) {
     const grid = document.createElement("div");
     grid.classList.add("grid");
     container.appendChild(grid);
 }
+
+const gridComponent = document.querySelectorAll(".grid");
+
+gridComponent.forEach((div) => {
+    div.addEventListener('mouseenter', () => {
+    div.style.backgroundColor = 'black';
+    });
+});
 
