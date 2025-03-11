9306c9668ca67c5bcf97cfb8bd00b7ea8d23a78a
ctaylor9107 <coleman.raphael.taylor@gmail.com>
2025-03-11 12:22:43 -0500

    Include button to update grid size
    I have added a button to the page so the users can update the dimensions
    of the grid.
    
    Currently this just updates the number of blocks on the page but breaks
    through the grid size.
    
    The next iteration should adjust the block size so they fit in the same
    grid despite having more squares.

--- etch_javascript.js
+++ etch_javascript.js
@@ -1,20 +1,50 @@
 
 const container = document.querySelector("#container");
-let gridAmount = 256;
+const button = document.querySelector("button");
+let gridAmount = 16;
 
 
 
-for (i = 0; i < gridAmount; i++) {
-    const grid = document.createElement("div");
-    grid.classList.add("grid");
-    container.appendChild(grid);
-}
 
-const gridComponent = document.querySelectorAll(".grid");
+function createGrid() {
 
-gridComponent.forEach((div) => {
-    div.addEventListener('mouseenter', () => {
-    div.style.backgroundColor = 'black';
+
+    for (let i = 0; i < gridAmount * gridAmount; i++) {
+        const grid = document.createElement("div");
+        grid.classList.add("grid");
+        container.appendChild(grid);
+    }
+
+    const gridComponent = document.querySelectorAll(".grid");
+
+    gridComponent.forEach((div) => {
+        div.addEventListener('mouseenter', () => {
+        div.style.backgroundColor = 'black';
+        });
     });
+};
+
+createGrid();
+
+
+
+
+button.addEventListener('click', () => {
+    gridAmount = prompt("Input how many squares you would like the grid to be across and down.");
+    if (gridAmount > 100) {
+        gridAmount = prompt("Please input a value of 100 or less")
+    }
+    else if (gridAmount <= 100) {
+        let deleteGrid = document.querySelectorAll(".grid");
+        deleteGrid.forEach((div) => {
+            div.remove();
+        });
+        createGrid();
+    };
 });
+
+
+
+
+
 
